7998807f8dc1bfd3a83b3e3353e3b9aa7d4b5b4b
mikestratton <mike@mikestratton.net>
2023-04-19 19:29:31 -0400

    controller, models, migrations and seeder to meet MVP

---
+++ app/Http/Controllers/ProgrammingLanguageController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProgrammingLanguage;
+use App\Http\Requests\StoreProgrammingLanguageRequest;
+use App\Http\Requests\UpdateProgrammingLanguageRequest;
+
+class ProgrammingLanguageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProgrammingLanguageRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProgrammingLanguage $programmingLanguage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProgrammingLanguage $programmingLanguage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProgrammingLanguageRequest $request, ProgrammingLanguage $programmingLanguage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProgrammingLanguage $programmingLanguage)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/RoleUserController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RoleUser;
+use App\Http\Requests\StoreRoleUserRequest;
+use App\Http\Requests\UpdateRoleUserRequest;
+
+class RoleUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRoleUserRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RoleUser $roleUser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RoleUser $roleUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRoleUserRequest $request, RoleUser $roleUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RoleUser $roleUser)
+    {
+        //
+    }
+}
